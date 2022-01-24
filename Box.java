3048e3d04dc86948a0404b6b9e71f4aad183e5a7
MaryLensin <88140591+MaryLensin@users.noreply.github.com>
2022-01-24 09:23:23 +0200

    Add files via upload

---
+++ Box.java
@@ -0,0 +1,19 @@
+package domain;
+
+import custom.HasValue;
+
+//the target
+
+@HasValue
+public class Box {
+
+	//public String value;
+
+}
+
+@HasValue
+class Container {
+
+	//public String value;
+
+}
